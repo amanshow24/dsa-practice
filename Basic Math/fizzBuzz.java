c2ac4e47cb580181511b11e0f800d33ed357f668
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-05-26 18:43:45 +0530

    412. Fizz Buzz

---
+++ Basic Math/fizzBuzz.java
@@ -0,0 +1,17 @@
+// 412. Fizz Buzz
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> ans = new ArrayList<>() ;
+
+        int i = 1 ;
+        while(i <= n){
+            if(i % 3 == 0 && i % 5 == 0) ans.add("FizzBuzz") ;
+            else if (i % 3 == 0) ans.add("Fizz") ;
+            else if (i % 5 == 0) ans.add("Buzz") ;
+            else ans.add(String.valueOf(i)) ;
+
+            i++ ;
+        }
+        return ans ;
+    }
+}
