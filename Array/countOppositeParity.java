7b2970828629bd3f5512a997a066784076bd64e6
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-05-27 11:04:07 +0530

    3917. Count Indices With Opposite Parity

---
+++ Array/countOppositeParity.java
@@ -0,0 +1,17 @@
+// 3917. Count Indices With Opposite Parity
+class Solution {
+    public int[] countOppositeParity(int[] nums) {
+        int n = nums.length ;
+        int[] ans = new int[n] ;
+
+        for(int i = 0 ; i < n ; i++){
+            int count = 0 ;
+            for(int j = i + 1 ; j < n ; j++){
+                if(nums[i] % 2 == 0 && nums[j] % 2 != 0) count++ ;
+                else if (nums[i] % 2 != 0 && nums[j] % 2 == 0) count++ ;
+            }
+            ans[i] = count ;
+        }
+        return ans ;
+    }
+}
