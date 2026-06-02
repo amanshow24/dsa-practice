db8722dc624dfe6e17db50c66b3df970d64d6d7c
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-06-02 15:32:38 +0530

    1051. Height Checker

---
+++ Sorting-Algo/heightChecker.java
@@ -0,0 +1,20 @@
+// 1051. Height Checker
+class Solution {
+    public int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] expected = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (heights[i] != expected[i])
+                ans++;
+        }
+
+        return ans;
+    }
+}
