bec8416abb41a50dc7b279f56692d01da8db877c
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-06-05 21:58:11 +0530

    3945. Digit Frequency Score - simplify
    Refactor digitFrequencyScore method to simplify logic and remove unused frequency array.

--- Basic Math/digitFrequencyScore.java
+++ Basic Math/digitFrequencyScore.java
@@ -2,28 +2,14 @@
 
 class Solution {
     public int digitFrequencyScore(int n) {
-        int[] freq = new int[10] ;
-
-        int org = n ;
-        int temp = n ;
+        int ans = 0 ;
         
         while(n != 0){
             int digit = n % 10 ;
-            freq[digit]++ ;
+            ans += digit ;
 
             n /= 10 ;
         }
-
-        int ans = 0 ;
-        while(temp != 0){
-            int digit = temp % 10 ;
-
-            ans += digit * freq[digit] ;
-            freq[digit] = 0 ;
-
-            temp /= 10 ;
-        }
-
         return ans ;
     }
 }
