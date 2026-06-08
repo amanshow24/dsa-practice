2824f5ab4cd543cb6896b38279e710547f204b9a
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-06-08 16:42:32 +0530

    2161. Partition Array According to Given Pivot

---
+++ Array/pivotArray.java
@@ -0,0 +1,21 @@
+// 2161. Partition Array According to Given Pivot
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n] ;
+
+        int left = 0 , right = n - 1 ;
+
+        for(int i = 0 , j = n - 1 ; i < n ; i++ , j--){
+            if(nums[i] < pivot) result[left++] = nums[i] ;
+
+            if(nums[j] > pivot) result[right--] = nums[j] ;
+        }
+
+        while(left <= right){
+            result[left++] = pivot ;
+        }
+        
+        return result;
+    }
+}
