0f4f71c13c432eb0f3dc266c07bd5412c1cae683
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-06-16 11:24:04 +0530

    3612. Process String with special operation 1

---
+++ StringBuilder/processStr.java
@@ -0,0 +1,27 @@
+// 3612. Process String with special operation 1 
+class Solution {
+    public String processStr(String s) {
+        int n = s.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            int idx = ch - 96;
+
+            if (idx > 0 && idx < 27) {
+                sb.append(ch);
+            } else if (ch == '*') {
+                if (sb.length() > 0)
+                    sb.deleteCharAt(sb.length() - 1);
+            } else if (ch == '#') {
+                sb.append(sb.toString());
+            } else if (ch == '%') {
+                sb = sb.reverse();
+            }
+        }
+
+        return sb.toString();
+    }
+}
