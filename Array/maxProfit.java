049dc78605ef5888de5658fa3cdb9e7f640925c3
Aman Show <144575448+amanshow24@users.noreply.github.com>
2026-07-06 17:37:56 +0530

    122. Best Time to Buy and Sell Stock II

---
+++ Array/maxProfit.java
@@ -0,0 +1,15 @@
+// 122. Best Time to Buy and Sell Stock II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length ;
+        int ans = 0 ;
+
+        for(int i = 1 ; i < n ; i++){
+            if(prices[i] > prices[i - 1]){
+                ans += prices[i] - prices[i - 1] ;
+            }
+        }
+
+        return ans ;
+    }
+}
